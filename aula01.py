15aebbd8efd04f637ef4e2c58098221a4da54927
gustavosha <116170062+gustavosha@users.noreply.github.com>
2024-03-27 10:49:59 -0300

    Add files via upload

---
+++ aula01.py
@@ -0,0 +1,20 @@
+import os
+
+#print ("Olá mundo cruel!")
+
+#print ("O 3DS é a turma número 1 do CESF")
+
+'''turma = "2DS"
+pos = 2
+print ("O", turma, "é a turma número", pos, "do CESF")
+print(type(turma))
+print(type(pos))'''
+
+turma = "3DS"
+pos = 1
+print ("O", turma, "é a turma número", pos, "do CESF")
+print(type(turma))
+print(type(pos))
+os.system("clear")
+
+
